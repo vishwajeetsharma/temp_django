41da6f65fabea140d91d8547a761c2a9aeaa70cd
vishwajeetsharma <vishwajeetsharma9821593401@gmail.com>
2021-12-10 18:09:16 +0530

    day 2.2

--- temp/urls.py
+++ temp/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-# from .views import index, about
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('about/', views.about),
+    path('npjv/', include('npjv.urls'))
 ]
